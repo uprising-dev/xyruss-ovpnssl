5315885b04cf50e0fc0d9701d1e5ff19fc3726e4
uprising-dev <58985081+uprising-dev@users.noreply.github.com>
2020-08-25 21:28:55 +0800

    Update ovpnssl.sh

--- ovpnssl.sh
+++ ovpnssl.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-cat cat <<EOT >> test
-line 1
-line 2
+cat <<EOT >> /etc/stunnel/stunnel.conf
+
+[openvpn]
+accept = 445
+connect = 127.0.0.1:110
+cert = /etc/stunnel/stunnel.pem
 EOT
+
+cd
+rm -f ovpnssl.sh
+echo "VPS will reboot now ..."
+restart
